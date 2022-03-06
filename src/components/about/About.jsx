7201d04315dc0e962b0dbd8ab801d64b2e7a5c60
MatheusBlanco <msallesblanco@gmail.com>
2022-03-06 19:53:38 -0300

    Deploy test 2

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import me from "../../assets/PXL_20211029_130812167.jpg";
 
 const About = () => {
   return (
     <section id="about">
       <h5>Get to know</h5>
-      <h2>About</h2>
+      <h2>About me</h2>
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={me} alt="About me" />
+          </div>
+        </div>
+        <div className="about__content"></div>
+      </div>
     </section>
   );
 };
