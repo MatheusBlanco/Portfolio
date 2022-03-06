1d3e7a279000dbee80fb7f6afbbdc36972cce8c1
MatheusBlanco <msallesblanco@gmail.com>
2022-03-06 20:25:57 -0300

    About me section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,18 +1,54 @@
 import React from "react";
 import me from "../../assets/PXL_20211029_130812167.jpg";
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { VscFolderLibrary } from "react-icons/vsc";
+import "./about.css";
 
 const About = () => {
   return (
     <section id="about">
       <h5>Get to know</h5>
       <h2>About me</h2>
+
       <div className="container about__container">
         <div className="about__me">
           <div className="about__me-image">
             <img src={me} alt="About me" />
           </div>
         </div>
-        <div className="about__content"></div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className="about__icon" />
+              <h5>Experience</h5>
+              <small>1+ year of experience</small>
+            </article>
+
+            <article className="about__card">
+              <FiUsers className="about__icon" />
+              <h5>Clients</h5>
+              <small>1+ year of experience</small>
+            </article>
+
+            <article className="about__card">
+              <VscFolderLibrary className="about__icon" />
+              <h5>Projects</h5>
+              <small>2 projects</small>
+            </article>
+          </div>
+
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate
+            ad odio, voluptates, praesentium voluptate pariatur eius, veniam sed
+            enim magni excepturi. Ratione officiis quae quos, iste molestias
+            dignissimos nam doloremque.
+          </p>
+          <a href="#contact" className="btn btn-primary">
+            Let's talk
+          </a>
+        </div>
       </div>
     </section>
   );
